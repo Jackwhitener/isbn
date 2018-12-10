f58bf705e94840ad9c032032d2a6bc0ea0195999
Jackwhitener <jackwhitener1@gmail.com>
2018-12-10 16:46:48 -0500

    Adds front-end junk

--- app.rb
+++ app.rb
@@ -3,5 +3,21 @@ require_relative 'isbn10_checker.rb'
 require_relative 'isbn13_checker.rb'
 
 get '/' do
-erb :home
+	erb :home
+end
+post '/result' do
+	number = params[:number]
+	isbntype = params[:isbntype]
+	if isbntype == '10'
+		valid = checkten(number)
+	elsif isbntype == '13'
+		valid = checkthirteen(numberlengththirteen(number),number)
+	end
+	redirect '/result?number=' + number + '&isbntype=' + isbntype + '&valid=' + valid.to_s
+end
+get '/result' do
+	number = params[:number]
+	isbntype = params[:isbntype]
+	valid = params[:valid]
+	erb :result, locals: {number: number, isbntype: isbntype, valid: valid}
 end
